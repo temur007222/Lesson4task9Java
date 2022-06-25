e3e2aa6490939454b6cb67eaf46398c1d8ef65ba
temur007222 <temurxaydarov1@gmail.com>
2022-06-25 12:27:12 +0500

    Initial commit

---
+++ app/src/main/java/com/example/lesson4task9java/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.lesson4task9java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView iv_image;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    void initViews(){
+        iv_image = findViewById(R.id.iv_image);
+        button = findViewById(R.id.b_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bounce);
+                iv_image.startAnimation(animation);
+            }
+        });
+    }
+}
